632de2608b31de49bd604aa04ab470e782f517fd
Betzabeth Narvaez <betzynarv@gmail.com>
2021-07-29 16:30:51 -0500

    add original homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Cute Study Pal</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Cute Study Pal!
-        </p>
-      </header>
+      
+      {/* title and pic (changes based on state of timer) */}
+      <h1 id="pomodoro-title">Ready?</h1>
+      <img id="pomodoro-pic" src="https://cute-study-pal.s3.us-east-2.amazonaws.com/before-start.png" alt="Ready?" />
+
+      {/* pomodoro timer */}
+      <div id="timer">00:00</div>
+
+      {/* start, pause, and restart */}
+      <div id="timer-buttons">
+        <button className="bg-ldthistle hover:bg-dthistle font-bolf py-1 px-3 rounded-full">
+          Start
+        </button>
+        <div className="divider"/>
+        <button className="bg-ldthistle hover:bg-dthistle font-bolf py-1 px-3 rounded-full">
+          Pause
+        </button>
+        <div className="divider"/>
+        <button className="bg-ldthistle hover:bg-dthistle font-bolf py-1 px-3 rounded-full">
+          Restart
+        </button>
+      </div>
+
+      {/* to do list */}
+
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: theme => ({
+        'ldthistle': '#BAA1BA',
+        'dthistle': '#886F88',
+        'gblue': '#BFD9D9',
+        'gyello': '#D9D9BF'
+      })
+    },
   },
   variants: {
     extend: {},
